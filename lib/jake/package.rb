f4e6d4cacc4cc5f34fe603f7519a8d6ffe56ddd3
James Coglan <jcoglan@googlemail.com>
2008-07-04 15:54:08 +0100

    Adding Holly @require comment support, configurable through jake.yml.

--- lib/jake/package.rb
+++ lib/jake/package.rb
@@ -9,6 +9,11 @@ module Jake
       @minified ||= Packr.pack(source, packer_settings)
     end
     
+    def header
+      reqs = @config[:requires] || []
+      [super, *reqs.map { |r| "// @require #{r}" }].join("\n")
+    end
+    
   end
 end
 
