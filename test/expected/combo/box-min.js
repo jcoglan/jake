a1879b77c736c619e896b7d1fc3d83109783a92f
James Coglan <jcoglan@googlemail.com>
2009-06-09 15:00:35 +0100

    Tests for bundles built using output from package builds.

---
+++ test/expected/combo/box-min.js
@@ -0,0 +1,8 @@
+/**
+ * Jake test files should all include this
+ * even if they have been minified.
+ * 0.5.0-min
+ **/
+
+Foo=(function(b,c){var a=true;return{_0:null,field:a,global:foo}})(window,something);
+Basic={VERSION:"0.5.0-min",function(a){var b=4;return b+this._foo+a}};Basic.Ext="MIN";
---
+++ test/expected/combo/box.js
@@ -0,0 +1,28 @@
+/**
+ * Jake test files should all include this
+ * even if they have been minified.
+ * 0.5.0-src
+ **/
+
+Foo = (function(my, vars) {
+  var another = true;
+  
+  return {
+    _priv:  null,
+    field:  another,
+    global: foo
+  };
+})(window, something);
+
+
+Basic = {
+  VERSION:  "0.5.0-src",
+  
+  function(something) {
+    var myVar = 4;
+    return myVar + this._foo + something;
+  }
+};
+
+
+Basic.Ext = "SRC";
